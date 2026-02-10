5103dd49fbf382bea6224dc61c7a0ac09a3746be
tpoisot <timothee.poisot@umontreal.ca>
2026-02-10 14:51:05 -0500

    fix commit

--- travail.jl
+++ travail.jl
@@ -12,7 +12,6 @@
 #      github: DeuxiAut
 # ---
 
-
 # # Introduction
 
 import Random
@@ -20,8 +19,25 @@ Random.seed!(123456)
 
 # # Présentation du modèle
 
+"""
+    foo(x, y)
+
+Cette fonction ne fait rien.
+"""
+function foo(x, y)
+    return nothing
+end
+
 # # Implémentation
 
 # # Présentation des résultats
 
+# La figure suivante représente des valeurs aléatoires:
+
+hist(randn(100))
+
 # # Discussion
+
+# On peut aussi citer des références dans le document `references.bib`,
+# @ermentrout1993cellular -- la bibliographie sera ajoutée automatiquement à la
+# fin du document.
